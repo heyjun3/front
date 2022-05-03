56b09a2fb7aa698c82a549fccb7182ed0ff461fa
jun <t3k2g5kako@yahoo.co.jp>
2022-05-03 12:43:48 +0900

    add components

--- src/src/App.js
+++ src/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {Grid} from '@material-ui/core'
+import Header from './components/Header'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container direction="column">
+        <Grid item>
+          <Header />
+        </Grid>
+      </Grid>
     </div>
   );
 }
