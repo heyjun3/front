f40a700cbe2d877bb6133812941793143e944923
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-05-06 16:41:41 +0900

    add sign in

--- src/src/App.js
+++ src/src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import {Grid} from '@material-ui/core'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
+import SignIn from './templates/SignIn'
+import SignUp from './templates/SignUp'
 
 
 function App() {
   return (
     <div className="App">
-      <Grid container direction="column">
-        <Grid item>
-          <Header />
-        </Grid>
-      </Grid>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />}/>
+          <Route path="/" element={<SignUp />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/src/components/Header.js
+++ src/src/components/Header.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+import {Button} from '@material-ui/core';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+// import Link from '@mui/material/Link';
+import {Link} from 'react-router-dom';
 
-const Header = () => {
+export default function ButtonAppBar() {
   return (
-    <div>Header</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography component="div" sx={{ flexGrow: 1 }}>
+            <Button component={Link} to="/" color="inherit">Search</Button>
+          </Typography>
+          <Button component={Link} to="/sign-in" color="inherit">Sign in</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Header
