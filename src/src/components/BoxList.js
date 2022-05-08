c68966e68853e015023bd393071baa147090f3a6
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-05-08 14:59:27 +0900

    add box list

--- src/src/components/BoxList.js
+++ src/src/components/BoxList.js
@@ -8,7 +8,7 @@ import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import CommentIcon from '@mui/icons-material/Comment';
 import DeleteIcon from '@mui/icons-material/Delete';
-import '../App.css';
+import {Link} from 'react-router-dom';
 
 
 export default function BoxList() {
@@ -19,7 +19,16 @@ export default function BoxList() {
     .then(data =>
         setFilenames(data['list'])
       )
-  })
+  }, [])
+
+  const deleteFile = (value) => {
+    setFilenames(
+      filenames.filter((file) => (file !== value))
+    )
+    fetch(`http://localhost:5000/deleteFile/${value}`, {method: 'DELETE', mode: 'cors'})
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
 
   return (
     <List sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper', margin: 'auto', mt: '10px' }}>
@@ -30,13 +39,13 @@ export default function BoxList() {
           <ListItem
             key={value}
             secondaryAction={
-              <IconButton edge="end" aria-label="comments">
+              <IconButton edge="end" aria-label="comments" onClick={() => deleteFile(value)}>
                 <DeleteIcon />
               </IconButton>
             }
             disablePadding
           >
-            <ListItemButton role={undefined} dense>
+            <ListItemButton component={Link} to="/" role={undefined} dense >
               <ListItemText align="center" id={labelId} primary={value} />
             </ListItemButton>
           </ListItem>
