3b14e77cd367925a832caa7fd8a81e1d14ba0268
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-05-10 10:49:20 +0900

    add handlesubmit

--- src/src/components/Search.js
+++ src/src/components/Search.js
@@ -8,26 +8,38 @@ import SearchIcon from '@mui/icons-material/Search';
 import DirectionsIcon from '@mui/icons-material/Directions';
 
 export default function CustomizedInputBase() {
+  const [info, setInfo] = React.useState('')
 
-  const getItemInfo = (value) => {
-      fetch(`http://localhost/search/${value}`, {method: 'GET', mode: 'cors'})
-      .then(res => res.json())
-      .then(data => console.log(data))
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget)
+      const value = data.get('amazon')
+      console.log(value)
+      setInfo(value)
+    //   fetch(`http://localhost/search/${value}`, {method: 'GET', mode: 'cors'})
+    //   .then(res => res.json())
+    //   .then(data => console.log(data))
   }
   return (
+    <div>
     <Paper
       component="form"
+      onSubmit={handleSubmit}
       sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '50%' , margin: 'auto', mt: '50px'}}
     >
       <InputBase
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search Amazon"
         inputProps={{ 'aria-label': 'search amazon' }}
+        name='amazon'
       />
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
       <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
         <SearchIcon />
       </IconButton>
     </Paper>
+    {info ? <p>{info}</p> : <p>aaa</p>}
+    </div>
+    
   );
 }
