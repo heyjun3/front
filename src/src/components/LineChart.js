b5a948673d2ec435da475f9b336435b3500104b1
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-05-17 04:38:16 +0900

    fix css

--- src/src/components/LineChart.js
+++ src/src/components/LineChart.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import {LineChart, Line, CartesianGrid, XAxis, YAxis} from 'recharts';
+import {LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer} from 'recharts';
+
 
 const RenderLineChart = (props) => {
   return (
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '70vh'}}>
-      <LineChart width={1000} height={400} data={props.data} >
+    <div>
+      <ResponsiveContainer className="LineChart" width='70%' height={400}>
+      <LineChart data={props.data} >
         <Line type="monotone" dataKey="price" stroke="#8884d8" dot={false}/>
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <XAxis dataKey="date" />
         <YAxis />
       </LineChart>
+      </ResponsiveContainer>
     </div>
   )
 }
