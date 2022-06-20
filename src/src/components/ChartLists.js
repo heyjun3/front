3163118915df320337287d18e2129a4355fbf0e5
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-06-20 14:52:58 +0900

    fix linechart

--- src/src/components/ChartLists.js
+++ src/src/components/ChartLists.js
@@ -3,10 +3,10 @@ import {useLocation} from 'react-router-dom'
 import RenderLineChart from './LineChart'
 
 const jan = '4901872837144'
-const asin = 'B00YM1MEJI'
+const asin = 'B08K3GYGQ9'
 
 const ChartLists = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState({})
 
   useEffect(() => {
     fetch(`http://localhost:5000/search/${asin}`, {method: 'GET', mode: 'cors'})
@@ -16,8 +16,8 @@ const ChartLists = () => {
 
   return (
     <div className="chartLists">
-        <RenderLineChart data={data.chart_data} title={data.title} jan={jan} asin={asin}/>
-        <RenderLineChart data={data.chart_data} title={data.title} jan={jan} asin={asin}/>
+        <RenderLineChart data={data.data} title={data.title} jan={jan} asin={asin}/>
+        <RenderLineChart data={data.data} title={data.title} jan={jan} asin={asin}/>
     </div>
   )
 }
--- src/src/components/LineChart.js
+++ src/src/components/LineChart.js
@@ -8,10 +8,12 @@ const RenderLineChart = (props) => {
       <h3 className="title">{props.title}</h3>
       <ResponsiveContainer className="LineChart" width='70%' height={400}>
       <LineChart data={props.data} >
-        <Line type="monotone" dataKey="price" stroke="#8884d8" dot={false}/>
+        <YAxis yAxisId={1} type="number" domain={["dataMin - 1000", "dataMax + 1000"]} label={{value: "price", dy: 20}}/>
+        <YAxis yAxisId={2} type="number" domain={["dataMin - 1000", "dataMax + 2000"]} orientation="right" label={{value: "rank", dy: 20}}/>
+        <Line yAxisId={1} type="monotone" dataKey="price" stroke="#8884d8" dot={false}/>
+        <Line yAxisId={2} type="monotone" dataKey="rank" stroke="#82ca9d" dot={false}/>
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <XAxis dataKey="date" />
-        <YAxis />
       </LineChart>
       </ResponsiveContainer>
       <div className="asinBox"><h3 className="asin">JAN : 4901872837144 ASIN : <a href={`https://www.amazon.co.jp/dp/${props.asin}`} target="_blank" rel="noreferrer">{props.asin}</a></h3></div>
